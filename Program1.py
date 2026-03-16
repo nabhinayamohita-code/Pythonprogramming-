11fd3c7785c1d046866c4f691ea812ff25b48058
nabhinayamohita-code <nabhinayamohita@gmail.com>
2026-03-16 19:59:49 +0530

    Add files via upload

---
+++ Program1.py
@@ -0,0 +1,4 @@
+#This is bqsic print statement
+print("I'm Abhinaya")
+print("I like Pizza")
+print("It's really good")
---
+++ program2.py
@@ -0,0 +1,8 @@
+#variable= A variable behaves as if it was the value it contains
+# (strings,integer,float,boolean)
+
+first_name = "Bro"
+print()
+
+#f-string: begin a string with f or F before the openong quotation mark or triple quotation mark
+#by this ,inside the string you can write python expression between { and } characters
