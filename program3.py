50f449be65b253af6cd1354e2d3d385551c8f4f5
nabhinayamohita-code <nabhinayamohita@gmail.com>
2026-03-16 20:48:35 +0530

    Add files via upload

---
+++ program3.py
@@ -0,0 +1,22 @@
+#INTEGERS
+age = 25
+quantity = 3
+num_of_students = 30
+
+#Float
+price = 10.99
+distance = 5.5
+calories = 50
+print(f"The price is ${price}")
+print(f"you ran {distance} km today")
+print(f"you burnt {calories} today")
+
+#Boolean
+is_student = True
+print(f"Are you a student?:{is_student}")
+
+if is_student:
+    print("you are a student")
+else:
+    print("you are not a student")
+
